1928a311f20274a5c43aa86513d5fd3884ce2ee9
Yumantha <yumanthadesilva@gmail.com>
2017-12-17 10:03:47 +0530

    Merge routes

--- routes/routes.js
+++ routes/routes.js
@@ -25,27 +25,24 @@ router.post("/new", (req, res)=>{
         console.log(routeStops);
     }
 
-    const sql = "INSERT INTO route(route_num, start, end) " +
-        "VALUES(\"" + routeNum + "\", \"" + routeStart + "\", \"" + routeEnd + "\");";
+    var sql = "INSERT INTO route(route_num, start, end) VALUES (?, ?, ?);";
 
-    db.query(sql, (error, result)=>{
+    db.query(sql, [routeNum, routeStart, routeEnd],(error, result)=>{
         if(error){
             console.log(error);
             return res.json({success: false, msg: "Failed to add route to the system"});
         } else{
-            var sql2 = "SELECT route_id " +
-                "FROM route " +
-                "WHERE route_num=\"" + routeNum + "\" AND starT=\"" + routeStart + "\" AND end=\"" + routeEnd + "\";";
+            var sql2 = "SELECT route_id FROM route WHERE route_num=? AND start=? AND end=?;";
 
-            db.query(sql2, function(error, foundID){
+            db.query(sql2, [routeNum, routeStart, routeEnd], function(error, foundID){
                 if(error){
                     console.log(error);
                     return res.json({success: false, msg: "Failed to add route to the system"});
                 } else{
                     var routeID = foundID[0].route_id;
                     for(var i=0; i<routeStops.length; i++){
-                        var sql3 = "INSERT INTO route_stop VALUES(\"" + routeID + "\", \"" + routeStops[i] + "\", " + (i+1) + ");";
-                        db.query(sql3, function(error, stop){
+                        var sql3 = "INSERT INTO route_stop VALUES (?, ?, ?);";
+                        db.query(sql3, [routeID, routeStops[i], (i+1)], function(error, stop){
                             if(error){
                                 console.log(error);
                                 return res.json({success: false, msg: "Failed to add route to the system"});
@@ -67,11 +64,9 @@ router.post("/search", (req, res)=>{
     const routeNum = req.body.routeNum;
     const city = req.body.city;
 
-    var sql1 = "SELECT DISTINCT route_id " +
-        "FROM route NATURAL LEFT JOIN route_stop " +
-        "WHERE route_num LIKE \"%" + routeNum + "%\" AND stop LIKE \"%" + city + "%\";";
+    var sql1 = "SELECT DISTINCT route_id FROM route NATURAL LEFT JOIN route_stop WHERE route_num LIKE %?% ANS stop LIKE %?%;";
 
-    db.query(sql1, (error, foundIDs)=>{
+    db.query(sql1, [routeNum, city], (error, foundIDs)=>{
         if(error){
             console.log(error);
             return res.json({success: false, msg: "Could not the complete the search"});
@@ -90,10 +85,10 @@ router.post("/search", (req, res)=>{
                         routeStops: []
                     };
 
-                    var sql2 = "SELECT * FROM route WHERE route_id=\"" + foundID.route_id + "\";";
-                    var sql3 = "SELECT * FROM route_stop WHERE route_id=\"" + foundID.route_id + "\" ORDER BY route_id ASC, stop_num ASC;;";
+                    var sql2 = "SELECT * FROM route WHERE route_id=?;";
+                    var sql3 = "SELECT * FROM route_stop WHERE route_id=? ORDER BY route_id ASC, stop_num ASC";
 
-                    db.query(sql2, (error, foundRoutes)=>{
+                    db.query(sql2, [foundID.route_id], (error, foundRoutes)=>{
                         if(error){
                             console.log(error);
                         } else{
@@ -103,7 +98,7 @@ router.post("/search", (req, res)=>{
                             route.routeStart = foundRoutes[0].start;
                             route.routeEnd = foundRoutes[0].end;
 
-                            db.query(sql3, (error, foundStops)=>{
+                            db.query(sql3, [foundID.route_id], (error, foundStops)=>{
                                 if(error){
                                     console.log(error);
                                 } else{
@@ -132,15 +127,15 @@ router.post("/search", (req, res)=>{
 router.post("/:id/delete", (req, res)=>{
     var routeID = req.params.id;
 
-    var sql1 = "DELETE FROM route WHERE route_id=" + routeID + ";";
-    var sql2 = "DELETE FROM route_stop WHERE route_id=" + routeID + ";";
+    var sql1 = "DELETE FROM route WHERE route_id=?;";
+    var sql2 = "DELETE FROM route_stop WHERE route_id=?;";
 
-    db.query(sql1, (error, deletedRoute)=>{
+    db.query(sql1, [routeID], (error, deletedRoute)=>{
         if(error){
             console.log(error);
             return res.json({success: false, msg: "The requested route couldn't be deleted"});
         } else{
-            db.query(sql2, (error, deletedStops)=>{
+            db.query(sql2, [routeID], (error, deletedStops)=>{
                 if(error){
                     console.log(error);
                     return res.json({success: false, msg: "The requested route stops couldn't be deleted"});
@@ -157,8 +152,8 @@ router.post("/:id/delete", (req, res)=>{
 router.get("/:id/edit", (req, res)=>{
     var routeID = req.params.id;
 
-    var sql1 = "SELECT * FROM route WHERE route_id=" + routeID + ";";
-    var sql2 = "SELECT * FROM route_stop WHERE route_id=" + routeID + " ORDER BY route_id ASC, stop_num ASC;";
+    var sql1 = "SELECT * FROM route WHERE route_id=?;";
+    var sql2 = "SELECT * FROM route_stop WHERE route_id=? ORDER BY route_id ASC, stop_num ASC;";
 
     var routeToEdit = {
         routeID: "",
@@ -168,7 +163,7 @@ router.get("/:id/edit", (req, res)=>{
         routeStops: []
     };
 
-    db.query(sql1, (error, foundRoute)=>{
+    db.query(sql1, [routeID], (error, foundRoute)=>{
         if(error){
             console.log(error);
             return res.json({success:false, msg:"The route couldn't be found"});
@@ -178,7 +173,7 @@ router.get("/:id/edit", (req, res)=>{
             routeToEdit.routeStart = foundRoute[0].start;
             routeToEdit.routeEnd = foundRoute[0].end;
 
-            db.query(sql2, (error, foundStops)=>{
+            db.query(sql2, [routeID], (error, foundStops)=>{
                 if(error){
                     console.log(error);
                     return res.json({success:false, msg:"The route stops couldn't be found"});
@@ -223,17 +218,17 @@ router.post("/:id/edit", (req, res)=>{
         console.log(routeStops);
     }
 
-    var sql1 = "UPDATE route SET route_num=\"" + routeNum + "\", start=\"" + routeStart + "\", end=\"" + routeEnd + "\" WHERE route_id=" + routeID + ";";
-    var sqlDel = "DELETE from route_stop WHERE route_id=" + routeID + ";";
+    var sql1 = "UPDATE route SET route_num=?, start=?, end=? WHERE route_id=?;";
+    var sqlDel = "DELETE from route_stop WHERE route_id=?;";
 
-    db.query(sql1, (error, updatedRoute)=>{
+    db.query(sql1, [routeNum, routeStart, routeEnd, routeID], (error, updatedRoute)=>{
         if(error){
             console.log(error);
             return res.json({success: false, msg: "The route couldn't be updated"});
         } else{
             console.log("The route successfully updated");
 
-            db.query(sqlDel, (error, deletedStops)=>{
+            db.query(sqlDel, [routeID], (error, deletedStops)=>{
                 if(error){
                     console.log(error);
                     return res.json({success: false, msg: "The stops couldn't be deleted"});
@@ -241,8 +236,8 @@ router.post("/:id/edit", (req, res)=>{
                     console.log("Stops deleted successfully");
 
                     for(var i=0; i<routeStops.length; i++){
-                        var sql3 = "INSERT INTO route_stop VALUES(\"" + routeID + "\", \"" + routeStops[i] + "\", " + (i+1) + ");";
-                        db.query(sql3, function(error, stop){
+                        var sql3 = "INSERT INTO route_stop VALUES(?, ?, ?);";
+                        db.query(sql3, [routeID, routeStops[i], (i+1)], function(error, stop){
                             if(error){
                                 console.log(error);
                                 return res.json({success: false, msg: "Failed to add stop to the database"});
